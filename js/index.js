e229736db19beb520fb8add695e7cc6e5390ddd5
Farida <farida.alin@yahoo.com>
2020-11-17 21:52:44 +0100

    navbar bg scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+const getWindowScrollY = () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 15) {
+        navbar.classList.add('custom-bg')
+
+    } else {
+        navbar.classList.remove('custom-bg')
+    }
+};
+
+window.addEventListener("scroll", getWindowScrollY);
+
+const hamburger = document.querySelector('.custom-toggler');
+
+const addBackground = () => {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('custom-bg')
+}
+
+hamburger.addEventListener('click', addBackground);
