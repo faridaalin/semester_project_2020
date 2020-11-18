4cf73ab698d96c339239935471a72951bc8480c3
Farida <farida.alin@yahoo.com>
2020-11-18 13:00:36 +0100

    index.html for edit page

--- js/elements/renderFeatured.js
+++ js/elements/renderFeatured.js
@@ -2,10 +2,10 @@ const renderFeatured = (products) => {
   const container = document.querySelector('.featured-container')
   products.forEach(product => {
     if (product.featured) {
-
+      console.log(product);
       const feauredProduct = ` <div class="col-sm-6 col-md-3 mb-5 pb-5">
         <div class="product-top">
-          <a href="">  <img src="${product.image_url}" class="card-img-top img-fluid" alt="${product.title}">
+          <a href="pages/edit.html?id=${product.id}">  <img src="${product.image_url}" class="card-img-top img-fluid" alt="${product.title}">
             <div class="overlay btn-container d-flex justify-content-center align-items-center">
               <button type="button" class="content-btn btn btn-outline-primary">View</button>
             </div>
@@ -14,7 +14,7 @@ const renderFeatured = (products) => {
         </div>
         
         <div class="product-bottom  pt-3">
-          <a href="http://"><h3 class="card-title mb-0">${product.title}</h3></a>
+          <a href="pages/edit.html?id=${product.id}"><h3 class="card-title mb-0">${product.title}</h3></a>
           <div class="feature-info__price d-flex flex-row align-items-center justify-content-between">
             <h5 class=" card-text  mb-0">NOK ${product.price}</h5>
             <i class="fav fa fa-heart-o"></i>
