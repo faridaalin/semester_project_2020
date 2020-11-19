aefc823ea6766988961d3dd5a182c2d0b90b6db7
Farida <farida.alin@yahoo.com>
2020-11-19 15:33:01 +0100

    edit product function

--- js/edit.js
+++ js/edit.js
@@ -1,5 +1,64 @@
+import { BASE_URL } from "./utils/settings.js";
 import { renderNavbar } from "./elements/renderNavbar.js";
 import { login } from "./ui/login.js";
 
 login();
 renderNavbar();
+
+// Get the querystring
+const queryString = window.location.search;
+
+//Parse the querystring
+const urlParam = new URLSearchParams(queryString);
+const id = urlParam.get("id");
+
+const title = document.querySelector('#name');
+const brand = document.querySelector('#brand');
+const price = document.querySelector('#price');
+const description = document.querySelector('#description');
+const imgUrl = document.querySelector('#url');
+const productID = document.querySelector('#id');
+const featured = document.querySelector('#featured');
+
+
+(async () => {
+    const URL = `${BASE_URL}/products/${id}`;
+    // Get all fields
+
+    try {
+        const res = await fetch(URL);
+        const product = await res.json();
+
+        console.log(product);
+
+        title.value = product.title
+        brand.value = product.title
+        price.value = product.price
+        description.value = product.description
+        imgUrl.value = product.image_url
+        productID.value = product.id
+        featured.checked = product.featured;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+)();
+
+const form = document.querySelectorAll('.edit-form');
+const submit = document.querySelector('.product-edit');
+
+submit.addEventListener('click', (e) => {
+e.preventDefault()
+
+const formData = new FormData(form[0]);
+
+console.log('FormData', formData.get('price'));
+console.log('clicked');
+})
+
+console.log(submit);
+
+
+
+
--- js/elements/renderAllProducts.js
+++ js/elements/renderAllProducts.js
@@ -1,24 +1,26 @@
-import { user} from '../utils/settings.js';
-import { getFromLocal } from '../utils/storage.js';
+import { user } from "../utils/settings.js";
+import { getFromLocal } from "../utils/storage.js";
 
-const loggedInUser = getFromLocal(user);
-const editBtn = loggedInUser && loggedInUser.role.type === "authenticated" ? ` <button type="button" class="btn btn-info btn-sm edit-btn"><a href="/edit.html">Edit</a></button>` : "";
+
 
 const renderAllProducts = (products) => {
-  const container = document.querySelector('.shop-container')
-  console.log(products);
-  products.forEach(product => {
 
-    
-      return container.innerHTML += `<div class="col-6 col-md-3 mb-5 pb-5">
+  const container = document.querySelector(".shop-container");
+  products.forEach((product) => {
+    const loggedInUser = getFromLocal(user);
+    const edit =
+      loggedInUser && loggedInUser.role.type === "authenticated"
+        ? ` <button type="button" class="btn btn-info btn-sm edit-btn"><a href="/edit.html?id=${product.id}">Edit</a></button>`
+        : "";
+  
+    return (container.innerHTML += `<div class="col-6 col-md-3 mb-5 pb-5">
       <div class="product-top">
-      ${editBtn}
+      ${edit}
       <a href="pdp.html?id=${product.id}">  <img src="${product.image_url}" class="card-img-top img-fluid" alt="${product.title}">
       <div class="overlay btn-container btn-container--shop d-flex justify-content-center align-items-center">
             <button type="button" class="content-btn  btn btn-outline-primary">View</button>
           </div>
         </a>
-
       <div class="product-bottom  pt-3">
         <a href="http://"><h3 class="card-title mb-0">${product.title}</h3></a>
         <div class="feature-info__price d-flex flex-row align-items-center justify-content-between">
@@ -26,9 +28,7 @@ const renderAllProducts = (products) => {
           <i class="fav fa fa-heart-o"></i>
         </div>
       </div>
-    </div>`
-
-
+    </div>`);
   });
 };
 
