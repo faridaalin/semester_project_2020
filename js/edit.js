124cd2c5818d96eca7ab7f919c58a3dd1d146859
Farida <farida.alin@yahoo.com>
2020-11-20 12:24:58 +0100

    edit function

--- js/edit.js
+++ js/edit.js
@@ -2,14 +2,13 @@ import { BASE_URL, user, userToken } from "./utils/settings.js";
 import { getFromLocal } from "./utils/storage.js";
 import { renderNavbar } from "./elements/renderNavbar.js";
 import { login } from "./ui/login.js";
-import {showMessage} from './helpers/showMessage.js';
-import {isImageUrlValid} from './helpers/isValidImageUrl.js'
+import { showMessage } from './helpers/showMessage.js';
+import { isImageUrlValid } from './helpers/isValidImageUrl.js'
 
 login();
 renderNavbar();
 
 const loggedUser = getFromLocal(user);
-console.log(loggedUser);
 if (!loggedUser) location.href = "/";
 
 
@@ -39,7 +38,6 @@ if (loggedUser && loggedUser.role.type === "authenticated") {
             const res = await fetch(URL);
             const product = await res.json();
 
-            console.log(product);
 
             title.value = product.title
             brand.value = product.title
@@ -59,8 +57,31 @@ if (loggedUser && loggedUser.role.type === "authenticated") {
     )();
 
 
-    const updateProduct = () => {
+    const updateProduct = async (obj) => {
+        const URL = `${BASE_URL}/products/${obj.id}`;
+      
+        const token =  getFromLocal(userToken);
+       
+        const options = {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        }
 
+        console.log(options);
+
+        try {
+            const res = await fetch(URL, options);
+            const updatedProduct = await res.json();
+            console.log(updatedProduct);
+        }
+
+        catch(error) {
+            console.log(error);
+        }
     }
 
 
@@ -69,38 +90,44 @@ if (loggedUser && loggedUser.role.type === "authenticated") {
 
     const handleFormEdit = (e) => {
         e.preventDefault();
-        const URL = `${BASE_URL}/products/${id}`;
+     
 
         const inputs = document.querySelectorAll('.edit-form input');
         const inputsArr = [...inputs];
 
         inputsArr.forEach(input => {
-            if(input.value.trim().length === 0) {
+            if (input.value.trim().length === 0) {
 
-               return   console.log("Empty fields are not allowed");
-                
-            } 
+                return console.log("Empty fields are not allowed");
 
-      
-
-            if(input.type === "url" && input.value.trim().length > 0 && !isImageUrlValid(input.value.trim())) {
-                return console.log("invalid img url");
-                
             }
 
-         
-           
+            if(input.type === "price" && isNaN(input.value.trim())) {
+                return console.log('not a number');
+            }
+
+
+
+            if (input.type === "url" && input.value.trim().length > 0 && !isImageUrlValid(input.value.trim())) {
+                return console.log("invalid img url");
+
+            }
+
+
+
         });
         const productObj = {
-            title
-        }
+            title: title.value,
+            brand: brand.value,
+            price: price.value,
+            description: description.value,
+            image_url: imgUrl.value,
+            id: productID.value,
+            featured: featured.checked,
+        };
 
-       
-     updateProduct()
-
-
-
-    }
+        updateProduct(productObj);
+    };
 
     form.addEventListener('submit', handleFormEdit);
 
