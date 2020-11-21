3ea6d3aca9f278b30351d7804353e4e49e1cb3ad
Farida <farida.alin@yahoo.com>
2020-11-21 09:56:07 +0100

    added clearTimeout to serach input

--- js/elements/renderNavbar.js
+++ js/elements/renderNavbar.js
@@ -43,46 +43,30 @@ export const renderNavbar = () => {
 
 
 let searchbar = "";
+if(pathname === "/shop.html") {
+  searchbar = `        <li class="nav-item w-75 mr-2 ml-auto">
+  <div class="search ml-auto">
+    <input class="form-control mr-sm-2" type="search"  id="search" placeholder="Search" aria-label="Search">
+    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd"
+        d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z" />
+      <path fill-rule="evenodd"
+        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+    </svg>
 
-  if(pathname === "/shop.html") {
-      searchbar = `<div class="collapse navbar-collapse inner-navbar flex-grow-1 justify-content-md-between py-4 py-lg-0" id="navbarSupportedContent">
-      <ul class="navbar-nav nav-left mr-auto mx-lg-0 ml-lg-auto flex-row justify-content-between align-items-center">
-        <li class="nav-item w-75 mr-2 ml-auto">
-          <div class="search ml-auto">
-            <input class="form-control mr-sm-2" type="search"  id="search" placeholder="Search" aria-label="Search">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z" />
-              <path fill-rule="evenodd"
-                d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
-            </svg>
-  
-          </div>
-        </li>
-        <li class="nav-item mr-2">
-          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-        </svg>
-        </li>
-        <li class="nav-item">
-  
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-fill" fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd"
-          d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
-      </svg>
-        </li>
-      </ul>
-    </div> ` ;
-    document.addEventListener("DOMContentLoaded", () => {
- 
-      showSearch()
-  
-    });
+  </div>
+</li>`
 
-  }
+document.addEventListener("DOMContentLoaded", () => {
+
+    showSearch()
+
+  
+});
+}
+
+
 
     return innerNav.innerHTML = `
     <div class="collapse navbar-collapse inner-navbar flex-grow-1 justify-content-md-between pt-5 pt-lg-0" id="navbarSupportedContent">
@@ -96,7 +80,26 @@ let searchbar = "";
      ${authLink}
     </ul>
   </div>   
-    ${searchbar}
+  <div class="collapse navbar-collapse inner-navbar flex-grow-1 justify-content-md-between py-4 py-lg-0" id="navbarSupportedContent">
+      <ul class="navbar-nav nav-left mr-auto mx-lg-0 ml-lg-auto flex-row justify-content-between align-items-center">
+      ${searchbar}
+
+        <li class="nav-item mr-2">
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+        </svg>
+        </li>
+
+        <li class="nav-item">
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-fill" fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd"
+          d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z" />
+      </svg>
+        </li>
+      </ul>
+    </div>
     `
 
 
--- js/ui/showSearch.js
+++ js/ui/showSearch.js
@@ -1,39 +1,76 @@
-import { allProducts} from '../utils/settings.js';
+import { allProducts } from '../utils/settings.js';
 import { getFromLocal } from '../utils/storage.js';
 import renderAllProducts from '../elements/renderAllProducts.js';
 
 export const showSearch = () => {
-    const shoes = getFromLocal(allProducts);
-    
-    const search = document.querySelector('#search');
-    const handleSearch = (e) => {
-  
+  const shoes = getFromLocal(allProducts);
 
-      setTimeout(() => {
-        
-        const searchTerm = e.target.value.trim().toLowerCase();
-        
-        let searchedShoes = [];
+  const search = document.querySelector('#search');
 
-        const filteredSearch = shoes.filter((shoe) => {
-            return shoe.title.toLowerCase().includes(searchTerm) || shoe.description.toLowerCase().includes(searchTerm)
-            
-        })
 
-        console.log(filteredSearch);
-     
+  let timeout = null;
 
-        if (filteredSearch.length > 0) {
-            renderAllProducts(filteredSearch)
-          } else {
-            const msg = `Sorry, we currently don't have ${searchTerm}`; 
-            renderAllProducts([], msg);
 
-          };
 
-      }, 500);
-     
-    };
-    search.addEventListener('input', handleSearch)
+  search.addEventListener('keyup',  (e) => {
 
-  }
+    clearTimeout(timeout);
+
+    timeout = setTimeout( () => {
+
+      const searchTerm = e.target.value.trim().toLowerCase();
+
+
+      const filteredSearch = shoes.filter((shoe) => {
+        return shoe.title.toLowerCase().includes(searchTerm) || shoe.description.toLowerCase().includes(searchTerm)
+
+      })
+
+
+
+
+      if (filteredSearch.length > 0) {
+        renderAllProducts(filteredSearch)
+      } else {
+        const msg = `Sorry, we currently don't have ${searchTerm}`;
+        renderAllProducts([], msg);
+
+      };
+
+    }, 1000);
+  });
+
+
+
+
+  // const handleSearch = (e) => {
+
+
+  //   setTimeout(() => {
+
+  //     const searchTerm = e.target.value.trim().toLowerCase();
+
+  //     let searchedShoes = [];
+
+  //     const filteredSearch = shoes.filter((shoe) => {
+  //         return shoe.title.toLowerCase().includes(searchTerm) || shoe.description.toLowerCase().includes(searchTerm)
+
+  //     })
+
+  //     console.log(filteredSearch);
+
+
+  //     if (filteredSearch.length > 0) {
+  //         renderAllProducts(filteredSearch)
+  //       } else {
+  //         const msg = `Sorry, we currently don't have ${searchTerm}`; 
+  //         renderAllProducts([], msg);
+
+  //       };
+
+  //   }, 500);
+
+  // };
+  // search.addEventListener('input', handleSearch)
+
+}
