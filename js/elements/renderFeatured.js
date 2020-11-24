1e340328551a9c136bd40455ef845f4a1cd45835
Farida <farida.alin@yahoo.com>
2020-11-24 22:32:27 +0100

    removed feaured render function

--- js/elements/renderFeatured.js
+++ js/elements/renderFeatured.js
@@ -2,14 +2,13 @@ import renderAllProducts  from "../elements/renderAllProducts.js";
 
 
 const renderFeatured = (products) => {
-  const container = document.querySelector('.featured-container')
   const featuredPropducts = products.filter(product => product.featured);
   
   if(featuredPropducts.length !== 0) {
-    renderAllProducts(featuredPropducts, "", container);
+    renderAllProducts(featuredPropducts, "", '.featured-container');
   } else {
     document.querySelector('.featured-title').textContent = "Trending";
-    renderAllProducts(products, "", container);
+    renderAllProducts(products, "", '.featured-container');
   }
  
 };
