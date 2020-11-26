7d1c99dd6f576dd5f682cbc569bc5f0203aecddd
Farida <farida.alin@yahoo.com>
2020-11-26 22:17:10 +0100

    footer stylling

--- js/ui/showNavbarBgOnScroll.js
+++ js/ui/showNavbarBgOnScroll.js
@@ -1,25 +1,46 @@
 export const showNavbarBgOnScroll = () => {
-    const getWindowScrollY = () => {
-        const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 15) {
-            navbar.classList.add('custom-bg')
+    const navbar = document.querySelector('.navbar');
+   
+        const getWindowScrollY = () => {
 
-        } else {
-            navbar.classList.remove('custom-bg')
-        }
-    };
+            if(location.pathname === "/" || location.pathname === "/home.html") {
+                if (window.scrollY > 15) {
+                    navbar.classList.add('custom-bg')
+        
+                } else {
+                    navbar.classList.remove('custom-bg')
+                }
 
-    window.addEventListener("scroll", getWindowScrollY);
+                const hamburger = document.querySelector('.custom-toggler');
+    
+                const addBackground = () => {
+                    if (window.scrollY > 15 === false) {
+                        navbar.classList.toggle('custom-bg')
+                    }
+            
+                }
+            
+                hamburger.addEventListener('click', addBackground);
+                return;
+                
+            } else {
+                if (window.scrollY > 15) {       
+                    navbar.style.boxShadow = "rgba(198, 212, 219, 0.86) 2px 2px 13px 0px"
+        
+                } else {
+                    navbar.style.boxShadow = ""
+                }
 
-    const hamburger = document.querySelector('.custom-toggler');
+             
+            }
+           
+            
+     
+        };
+    
+        window.addEventListener("scroll", getWindowScrollY);
+    
 
-    const addBackground = () => {
-        const navbar = document.querySelector('.navbar');
-        if (window.scrollY > 15 === false) {
-            navbar.classList.toggle('custom-bg')
-        }
+   
 
-    }
-
-    hamburger.addEventListener('click', addBackground);
 };
