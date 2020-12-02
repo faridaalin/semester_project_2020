19a07c467e962b0f3b6415b27500e69b7a7d8c00
Farida <farida.alin@yahoo.com>
2020-12-02 10:22:46 +0100

    styling filter section

--- js/elements/renderNavbar.js
+++ js/elements/renderNavbar.js
@@ -53,7 +53,7 @@ export const renderNavbar = () => {
     let searchbar = "";
     if (pathname === "/shop.html") {
       searchbar = `
-      <li class="nav-item search-box w-75">
+      <li class="nav-item search-box mx-0 pb-4 pb-lg-0 w-75">
         <div class="search ">
           <input class="form-control mr-sm-2" type="search"  id="search" placeholder="Search" aria-label="Search">
             <svg class="search-icon" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -82,19 +82,21 @@ export const renderNavbar = () => {
        ${authLink}
       </ul>
     </div>   
-    <div class="collapse navbar-collapse inner-navbar flex-grow-1 justify-content-md-between py-4 py-lg-0" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse inner-navbar flex-grow-1 justify-content-md-between pb-5 pb-lg-0" id="navbarSupportedContent">
         <ul class="navbar-nav nav-left mr-auto mx-lg-0 ml-lg-auto flex-column flex-lg-row justify-content-between align-items-center">
         ${searchbar}
-          <li class="nav-item favs-icon">
+          <div class="d-flex flex-md-row">
+          <li class="nav-item favs-icon px-2 px-lg-0">
           <a class="nav-link icon" href="/fav.html"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
         </svg><span class="counter">0</span></a>
           </li>
-          <li class="nav-item cart-icon">
+          <li class="nav-item cart-icon px-2 px-lg-0">
           <a class="nav-link icon"  href="/cart.html"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
         </svg><span class="counter">0</span></a>
           </li>
+          </div>
         </ul>
      </div>
       `;
