5f9ab1cacf60a0924a73c50939322a43e6f04090
Farida <farida.alin@yahoo.com>
2020-12-08 13:34:06 +0100

    start on file upload for images

--- js/add.js
+++ js/add.js
@@ -16,6 +16,8 @@ const user = getLoggedInUser();
 
 if (!user) location.href = "/";
 
+
+
 if (user && user.username === "admin") {
     const URL = `${BASE_URL}/products`;
     const token = getFromLocal(userToken);
@@ -29,9 +31,58 @@ if (user && user.username === "admin") {
     const category = document.querySelector(".category");
     const featured = document.querySelector('#featured');
 
+    const image_file = document.querySelector('#image_upload');
+    const clear_file = document.querySelector('.clear');
+    const previewContainer = document.querySelector('.preview');
+    const previewImage = previewContainer.querySelector('.preview__image');
+    const previewText = previewContainer.querySelector('.preview__text');
+    let data;
+
+    image_file.addEventListener('change', () => {
+        const currentFile = image_file.files[0];
+
+        console.log(currentFile);
+
+        if (!currentFile) {
+            previewContainer.innerHTML = "No files selected for upload"
+            //previewContainer.classList.add('error');
+            previewImage.setAttribute('src', "")
+            return;
+        } else {
+            const reader = new FileReader();
+            previewText.style.display = "none";
+            previewImage.style.display = "block";
+            previewContainer.classList.remove('error');
+            console.dir(image_file);
+
+            data = new FormData();
+            data.append('files', currentFile)
+            console.log(data);
+            reader.readAsDataURL(currentFile)
+
+            reader.addEventListener('load', () => {
+                console.log(reader);
+                previewImage.setAttribute('src', reader.result)
+            })
+
+            clear_file.addEventListener('click', () => {
+                image_file.value = "";
+                previewImage.setAttribute('src', "")
+                previewImage.style.display = "none";
+                previewText.style.display = "block";
+            })
+        }
+    })
+
+
+
+
+
     const handleNewproduct = (e) => {
         e.preventDefault();
         removeMessage('#msg');
+
+        console.log(data);
 
         const isValid = validateFields(".add-form .form-control");
         if (isValid === false || isValid === undefined) return;
@@ -46,10 +97,15 @@ if (user && user.username === "admin") {
             category: category.value,
             featured: featured.checked,
         }
+        const productObjectFile = {
+            image_file: image_file.value,
 
-        addNewProduct(URL, token, productObject);
-        removeValidationStyle(".add-form .form-control")
-        form.reset();
+        }
+
+        //addNewProduct(URL, token, productObject);
+        //addNewProduct(URL, token, productObject);
+        //removeValidationStyle(".add-form .form-control")
+        //form.reset();
 
     }
 
--- js/helpers/validateFields.js
+++ js/helpers/validateFields.js
@@ -1,6 +1,6 @@
-import {showMessage} from "../helpers/showMessage.js";
-import {removeMessage} from '../helpers/removeMessage.js'
-import {isImageUrlValid} from "../helpers/isValidImageUrl.js";
+import { showMessage } from "../helpers/showMessage.js";
+import { removeMessage } from '../helpers/removeMessage.js'
+import { isImageUrlValid } from "../helpers/isValidImageUrl.js";
 
 export const validateFields = (tag) => {
   removeMessage("#msg");
@@ -10,18 +10,17 @@ export const validateFields = (tag) => {
   let isValid = true;
 
   for (let i = 0; i < inputsArr.length; i++) {
-
     if (inputsArr[i].value.length === 0) {
       inputsArr[i].classList.add("is-invalid");
       isValid = false;
 
     } else if (inputsArr[i].type === "url" && inputsArr[i].value.length > 0 && !isImageUrlValid(inputsArr[i].value)) {
-      const imgFeedback = (document.querySelector(".img-feedback").innerHTML = "Invalid url");
+      document.querySelector(".img-feedback").innerHTML = "Invalid url";
       inputsArr[i].classList.add("is-invalid");
       isValid = false;
 
     } else if (inputsArr[i].name === "price" && isNaN(inputsArr[i].value)) {
-      const priceFeedback = (document.querySelector(".price-feedback").innerHTML = "Price must a digit");
+      document.querySelector(".price-feedback").innerHTML = "Price must a digit";
       inputsArr[i].classList.add("is-invalid");
       isValid = false;
 
@@ -30,6 +29,21 @@ export const validateFields = (tag) => {
       inputsArr[i].classList.add("is-valid");
     }
   }
+
+
+
+  // else if (inputsArr[i].type === "url" && inputsArr[i].value.length > 0 && isImageUrlValid(inputsArr[i].value) || inputsArr[i].type === "file" && inputsArr[i].value.length > 0) {
+  //   console.log('Choose either');
+  //   console.log('inputsArr[i].type === "url"', inputsArr[i].type === "url");
+  //   console.log('inputsArr[i].type === "file"', inputsArr[i].type === "file");
+  //   inputsArr[i].type === "url" && inputsArr[i].classList.add("is-invalid");
+  //   inputsArr[i].type === "file" && inputsArr[i].classList.add("is-invalid");
+  //   const msg = "Choose either image file upload or image url";
+  //   showMessage("danger", msg, "#msg");
+  //   isValid = false;
+  //   return;
+  // }
+
 
 
   if (isValid === false) {
